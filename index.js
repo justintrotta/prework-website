e16247d78fef621dfc967b17db8ced919e59cda0
Justin <justindavidtrotta@gmail.com>
2021-11-26 20:06:45 -0500

    Updated index,js background function

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ background = document.body.style.backgroundColor;
 
 function seizureWarning(){
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    background = `#${randomColor}`
+    background = `#${randomColor}`;
 }
 
-while (true){
-    setTimeout(seizureWarning(), 500)
+function partyTime(){
+    while (true){
+        setTimeout(seizureWarning(), 500)
+    }
 }
+
+
+document.addEventListener("keydown", partyTime())
