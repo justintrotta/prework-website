d3e9643e78ba6fb26565d5ceb8fc8fb794a6f640
Justin <justindavidtrotta@gmail.com>
2021-11-26 20:02:39 -0500

    Completed index.js function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+background = document.body.style.backgroundColor;
 
 function seizureWarning(){
-    const randomColor = Math.floor
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    background = `#${randomColor}`
 }
 
 while (true){
